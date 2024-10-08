a38d0fd41ca3852ca9fb39f40d06e7acea3486c1
Manny Verma <mannyverma11@gmail.com>
2024-10-08 17:53:04 -0600

    updating toggle button

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -3,4 +3,10 @@ const body = document.body;
 
 toggleButton.addEventListener('click', () => {
 	body.classList.toggle('darkMode');
+
+	if (document.body.classList.contains('darkMode')) {
+		toggleButton.textContent = 'Light Mode';
+	} else {
+		toggleButton.textContent = 'Dark Mode';
+	}
 });
