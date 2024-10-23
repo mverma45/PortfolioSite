f6487851d13b92c30c1c39cef14ae646cfabaa2b
Manny Verma <mannyverma11@gmail.com>
2024-10-23 14:50:58 -0600

    updating HTML,CSS and JS code

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -3,10 +3,4 @@ const body = document.body;
 
 toggleButton.addEventListener('click', () => {
 	body.classList.toggle('darkModeToggle');
-
-	if (document.body.classList.contains('darkModeToggle')) {
-		toggleButton.textContent = 'Light Mode';
-	} else {
-		toggleButton.textContent = 'Dark Mode';
-	}
 });
